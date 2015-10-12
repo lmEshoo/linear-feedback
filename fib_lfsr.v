5b494f9ffaf98723b01002ddbabb3aab5073da61
Lini Mestar <lmEshoo@users.noreply.github.com>
2015-10-12 16:08:42 -0700

    Update fib_lfsr.v

--- fib_lfsr.v
+++ fib_lfsr.v
@@ -1,7 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:45:16 10/12/2015 
+// Design Name: 
+// Module Name:    fib_lfsr 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 module fib_lfsr(
   input  clk,
   input  rst,
-  output reg [4:0] data;
+  output reg [4:0] data,
   input [4:0] seed
 );
 
