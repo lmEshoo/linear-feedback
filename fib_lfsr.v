4cb50f583ba23492fb026619ce6c378bdb528454
Lini Mestar <lmEshoo@users.noreply.github.com>
2015-10-12 15:10:02 -0700

    Update fib_lfsr.v

--- fib_lfsr.v
+++ fib_lfsr.v
@@ -1,33 +1,15 @@
 module fib_lfsr(
-
   input  clk,
-
   input  rst,
-
-
-
   output [4:0] data
 
 );
 
-
-reg data=5’b00111;
-
-
 wire feedback = data[4] ^ data[1] ;
 
-
-
 always @(posedge clk or negedge rst)
-
   if (~rst) 
-
-    data <= 5'h1f;
-
+    data <= 4'b0111;
   else
-
     data <= {data[3:0], feedback} ;
-
-
-
 endmodule
